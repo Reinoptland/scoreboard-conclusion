f38672866ea53cb6610c0a9a6104bd446dbf8238
Reinoptland <reinoptland@gmail.com>
2020-09-16 15:37:47 +0200

    Create controlled form add event listeners & handler

--- src/components/PlayerForm.tsx
+++ src/components/PlayerForm.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function AddPlayerForm() {
+export default function PlayerForm() {
+  const initialState = localStorage.getItem("name") || "";
+  const [name, setName] = useState(initialState);
+
+  console.log("FORM RENDERED");
+
+  const feedback = name.length < 3 ? "This is too short" : null;
+
   return (
-    <div className="AddPlayerForm">[TODO: the form to add a new player]</div>
+    <div className="PlayerForm">
+      <label>Name:</label>
+      <input
+        value={name}
+        onChange={(e) => {
+          localStorage.setItem("name", e.target.value);
+          setName(e.target.value);
+        }}
+      />
+      <span>{feedback}</span>
+      <button>Add player</button>
+    </div>
   );
 }
