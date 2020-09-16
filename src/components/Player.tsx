dafefceef0d46eeb5c166fb0e803c310e6192d6f
Reinoptland <reinoptland@gmail.com>
2020-09-16 13:51:52 +0200

    Display score in Player, sort by scores

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 type Props = {
+  id: number;
   name: string;
+  score: number;
 };
 
 export default function Player(props: Props) {
   return (
     <li className="Player">
-      <p>{props.name}</p>
+      <p>
+        {props.name}, Score {props.score}
+      </p>
     </li>
   );
 }
--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Player from "./Player";
+import AddPlayerForm from "./PlayerForm";
+
+type TPlayer = {
+  id: number;
+  name: string;
+  score: number;
+};
+
+const compareByScore = (playerA: TPlayer, playerB: TPlayer) => {
+  return playerB.score - playerA.score;
+};
 
 export default function Scoreboard() {
+  const [players, setPlayers] = useState<TPlayer[]>([
+    { id: 1, name: "Violeta", score: 11 },
+    { id: 2, name: "Eszter", score: 14 },
+    { id: 3, name: "Jeroen v2", score: 4 },
+    { id: 4, name: "Lisa", score: 42 },
+  ]);
+
+  const playersSorted =
+    // first "copy" the array
+    [...players]
+      // then sort it with the `compareByScore` callback function
+      .sort(compareByScore);
+
   return (
     <div className="Scoreboard">
+      <h1>Scoreboard</h1>
       <p>Player's scores:</p>
       <ul>
-        <Player name="Violeta" />
-        <Player name="Eszter" />
-        <Player name="Jeroen v2" />
-        <Player name="Lisa" />
+        {playersSorted.map((player) => {
+          return (
+            <Player
+              key={player.id}
+              id={player.id}
+              name={player.name}
+              score={player.score}
+            />
+          );
+        })}
       </ul>
+      <AddPlayerForm />
     </div>
   );
 }
